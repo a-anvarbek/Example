a93b444637cf5626c0cb2bb7e4330eebcf586ca8
Anvarbek <c1221sharp@gmail.com>
2025-07-11 15:02:55 +0500

    feat: add some pages

---
+++ src/pages/tours/ToursHome.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Wrapper = styled.div``;
+
+const ToursHome = () => {
+  return (
+    <>
+      <Wrapper>Signature Tours Home page</Wrapper>
+    </>
+  );
+};
+
+export default ToursHome;
